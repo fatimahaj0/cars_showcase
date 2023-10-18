8d63cacb05edf51976ef89159f6b5e4feceb5ebc
fatimahaj0 <fatima_haj00@outlook.com>
2023-10-18 17:53:09 +0200

    first commit

---
+++ components/index.ts
@@ -0,0 +1,23 @@
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import Footer from "./Footer";
+import NavBar from "./Navbar";
+import ShowMore from "./ShowMore";
+import SearchBar from "./Searchbar";
+import Hero from "./Hero";
+import SearchManufacturer from "./SearchManufacturer";
+
+export {
+  Hero,
+  CarCard,
+  CarDetails,
+  CustomButton,
+  CustomFilter,
+  Footer,
+  NavBar,
+  ShowMore,
+  SearchBar,
+  SearchManufacturer,
+};
